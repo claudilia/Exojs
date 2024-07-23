3ec24dbdd2740ad77f34dae3dd16cf8e32e8a182
claudilia <claudiliarofine@gmail.com>
2024-07-23 13:56:50 +0200

    first commit

---
+++ bonjour.js
@@ -0,0 +1,10 @@
+// Question
+
+let hermosa = prompt("Question: quel est ton nom?");
+let mira = prompt("Question: quel est ton prénom?");
+
+// reponse
+document.write(`<p>La reponse est :${hermosa} ${mira}`);
+
+
+
