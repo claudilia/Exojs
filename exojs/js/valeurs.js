028241a608089ff782411bdb21d236ce3cf837f1
Claudilia <claudiliarofine@gmail.com>
2024-07-24 11:15:14 +0200

    Add files via upload

---
+++ exojs/js/valeurs.js
@@ -0,0 +1,25 @@
+let a = 2
+a = a - 1
+console.log(`le resultat est : ${a}`);
+
+a++;
+console.log(`le resultat est : ${a}`);
+
+let b = 8;
+b += 2;
+console.log(`le resultat est : ${b}`);
+
+let c = a + b * b;
+console.log(`le resultat est : ${c}`);
+
+let d = a * b + b
+console.log(`le resultat est : ${d}`);
+
+let e = a * (b + b);
+console.log(`le resultat est : ${e}`);
+
+let f = a * b / a;
+console.log(`le resultat est : ${f}`);
+
+let g = b / a * a;
+console.log(`le resultat est : ${g}`);
